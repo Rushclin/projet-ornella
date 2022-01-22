ee2e7ced59210ba3f8114dbbbccbd80198b4c264
Rushclin <takamrushclin@gmail.com>
2022-01-22 09:49:30 +0100

    Mise en place du template

---
+++ Vues/Template/footer.php
@@ -0,0 +1,66 @@
+<footer class="footer-container">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 col-sm-6">
+                <div class="footer-left">
+                    <span>© 2015 <a href="http://themeforest.net/user/westilian">westilian</a></span>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6">
+                <div class="footer-right">
+                    <span class="footer-meta">Crafted with&nbsp;<i class="fa fa-heart"></i>&nbsp;by&nbsp;<a href="http://themeforest.net/user/westilian">westilian</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</section>
+<script src="Assets/js/lib/jquery.js"></script>
+<script src="Assets/js/lib/jquery-migrate.js"></script>
+<script src="Assets/js/lib/bootstrap.js"></script>
+<script src="Assets/js/lib/jquery.ui.js"></script>
+<script src="Assets/js/lib/jRespond.js"></script>
+<script src="Assets/js/lib/nav.accordion.js"></script>
+<script src="Assets/js/lib/hover.intent.js"></script>
+<script src="Assets/js/lib/hammerjs.js"></script>
+<script src="Assets/js/lib/jquery.hammer.js"></script>
+<script src="Assets/js/lib/jquery.fitvids.js"></script>
+<script src="Assets/js/lib/scrollup.js"></script>
+<script src="Assets/js/lib/smoothscroll.js"></script>
+<script src="Assets/js/lib/jquery.slimscroll.js"></script>
+<script src="Assets/js/lib/jquery.syntaxhighlighter.js"></script>
+<script src="Assets/js/lib/velocity.js"></script>
+<script src="Assets/js/lib/jquery-jvectormap.js"></script>
+<script src="Assets/js/lib/jquery-jvectormap-world-mill.js"></script>
+<script src="Assets/js/lib/jquery-jvectormap-us-aea.js"></script>
+<script src="Assets/js/lib/smart-resize.js"></script>
+<!--iCheck-->
+<script src="Assets/js/lib/icheck.js"></script>
+<script src="Assets/js/lib/jquery.switch.button.js"></script>
+<!--CHARTS-->
+<script src="Assets/js/lib/chart/sparkline/jquery.sparkline.js"></script>
+<script src="Assets/js/lib/chart/easypie/jquery.easypiechart.min.js"></script>
+<script src="Assets/js/lib/chart/flot/excanvas.min.js"></script>
+<script src="Assets/js/lib/chart/flot/jquery.flot.min.js"></script>
+<script src="Assets/js/lib/chart/flot/curvedLines.js"></script>
+<script src="Assets/js/lib/chart/flot/jquery.flot.time.min.js"></script>
+<script src="Assets/js/lib/chart/flot/jquery.flot.stack.min.js"></script>
+<script src="Assets/js/lib/chart/flot/jquery.flot.axislabels.js"></script>
+<script src="Assets/js/lib/chart/flot/jquery.flot.resize.min.js"></script>
+<script src="Assets/js/lib/chart/flot/jquery.flot.tooltip.min.js"></script>
+<script src="Assets/js/lib/chart/flot/jquery.flot.spline.js"></script>
+<script src="Assets/js/lib/chart/flot/jquery.flot.pie.min.js"></script>
+<!--Forms-->
+<script src="Assets/js/lib/jquery.maskedinput.js"></script>
+<script src="Assets/js/lib/jquery.validate.js"></script>
+<script src="Assets/js/lib/jquery.form.js"></script>
+<script src="Assets/js/lib/j-forms.js"></script>
+<script src="Assets/js/lib/jquery.loadmask.js"></script>
+<script src="Assets/js/lib/vmap.init.js"></script>
+<script src="Assets/js/lib/theme-switcher.js"></script>
+<script src="Assets/js/apps.js"></script>
+</body>
+
+<!-- Mirrored from www.lab.westilian.com/westilo-admin/fixed-width-view/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 22 Jan 2022 06:33:48 GMT -->
+
+</html>
---
+++ index.php
@@ -0,0 +1,1 @@
+<?php require_once 'Vues/Template/app.php';
